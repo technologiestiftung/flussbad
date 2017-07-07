bae56509b621c948cd02f845047008e865ee819b
abekayo <s61443@beuth-hochschule.de>
2017-07-07 21:24:18 +0200

    Add sketch of turbidity sensor

---
+++ develop/src/arduino/turbidity.ino
@@ -0,0 +1,15 @@
+/**
+ * Turbidity sensor
+ * Reference: https://www.dfrobot.com/wiki/index.php/Turbidity_sensor_SKU:_SEN0189
+ * 
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  Serial.println(voltage);
+  delay(1000);
+} 
