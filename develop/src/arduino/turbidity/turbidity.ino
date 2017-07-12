636e33cdd803b2b227e9fb75153196bb62ae874c
abekayo <s61443@beuth-hochschule.de>
2017-07-12 20:49:36 +0200

    Refine turbidity sketch: add unit Voltage

--- develop/src/arduino/turbidity/turbidity.ino
+++ develop/src/arduino/turbidity/turbidity.ino
@@ -10,6 +10,7 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(A0);
   float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  Serial.println(voltage);
+  Serial.print(voltage);
+  Serial.println(" V");
   delay(1000);
 } 
